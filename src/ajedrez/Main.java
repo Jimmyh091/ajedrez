1a4af4b369e22158ee9a4c05588b0b1e22e67e9f
Carlos Madrid <37978151+Nozferatu@users.noreply.github.com>
2024-04-04 19:13:27 +0200

    Add files via upload

---
+++ src/ajedrez/Main.java
@@ -0,0 +1,14 @@
+package ajedrez;
+
+/**
+ *
+ * @author Jaime
+ * @author Carlos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ajedrez perfectamente funcional xd");
+    }
+    
+}
